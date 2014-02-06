19785e544275afb3d623902f935168ad253eb8c6
otmosina <otmosina@gmail.com>
2014-02-06 13:30:06 +0700

    Add draft chart for widget analitics

---
+++ Gemfile
@@ -0,0 +1,7 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem "pry"
+gem 'sinatra'
+gem 'rest-client'
+gem 'oj'
--- dashboard.rb
+++ dashboard.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'rest-client'
 require 'oj'
+require 'pry'
 
 
 get '/data.json' do
@@ -9,7 +10,7 @@ get '/data.json' do
   url = 'http://tp.aviasales.ru/report.php?r=51&m=1&h=a8022265abe4a76106cebfebe2c14e0ec21c24ca&i=1&export=json'
   affiliates = Oj.load(RestClient.get(url))
 
-  result = { 
+  result = {
   	'affiliates' => {
   	  'today' => affiliates[0],
   	  'yesterday' => affiliates[1],
@@ -49,7 +50,7 @@ get '/plot_profit_data.json' do
   content_type :json
   url = 'http://tp.aviasales.ru/report.php?r=67&m=1&h=84a2889a1d53c79a1301ed8c96a611db5c86b1fb&i=1&export=json'
   affiliates_profit_plot = Oj.load(RestClient.get(url))
-  result = { 
+  result = {
     'profit' => {
       'dates' => affiliates_profit_plot[0].map{|array| array[1]},
       'values' => affiliates_profit_plot[0].map{|array| array[0]}
@@ -66,7 +67,7 @@ get '/plot_clicks_and_bookings_data.json' do
   content_type :json
   url = 'http://tp.aviasales.ru/report.php?r=68&m=1&h=45763f5654aae31c1cae9727db23e70feeb17cad&i=1&export=json'
   affiliates_bookings_and_clicks_plot = Oj.load(RestClient.get(url))
-  result = { 
+  result = {
     'bookings' => {
       'dates' => affiliates_bookings_and_clicks_plot[0].map{|array| array[1]},
       'values' => affiliates_bookings_and_clicks_plot[0].map{|array| array[0]}
@@ -74,6 +75,32 @@ get '/plot_clicks_and_bookings_data.json' do
     'clicks' => {
       'dates' => affiliates_bookings_and_clicks_plot[1].map{|array| array[1]},
       'values' => affiliates_bookings_and_clicks_plot[1].map{|array| array[0]}
+    }
+  }
+  Oj.dump(result)
+end
+
+get '/fetch_widget_analitics.json' do
+  content_type :json
+  url = 'http://tp.aviasales.ru/report.php?r=71&u1=12&m=1&h=bf7cddbb19843b15b06138169de0b06a15b28f85&export=json'
+
+  values_widget_events_and_searches = Oj.load(RestClient.get(url))
+  result = {
+    'inits' => {
+      'dates' => values_widget_events_and_searches.map{|values|values[0]},
+      'values' => values_widget_events_and_searches.map{|values|values[1]}
+    },
+    'shows' => {
+      'dates' => values_widget_events_and_searches.map{|values|values[0]},
+      'values' => values_widget_events_and_searches.map{|values|values[2]}
+    },
+    'leads' => {
+      'dates' => values_widget_events_and_searches.map{|values|values[0]},
+      'values' => values_widget_events_and_searches.map{|values|values[3]}
+    },
+    'searches' => {
+      'dates' => values_widget_events_and_searches.map{|values|values[0]},
+      'values' => values_widget_events_and_searches.map{|values|values[4]}
     }
   }
   Oj.dump(result)
